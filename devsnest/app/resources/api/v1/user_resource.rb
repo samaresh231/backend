5b989b4ce861746abb5b2b94a989eed87090a7f7
priyamishra2903 <priya.1721ec1106@kiet.edu>
2020-12-22 02:19:59 +0530

    updates

--- devsnest/app/resources/api/v1/user_resource.rb
+++ devsnest/app/resources/api/v1/user_resource.rb
@@ -3,11 +3,19 @@
 module Api
   module V1
     class UserResource < JSONAPI::Resource
-      attributes :email, :name, :kind, :bio, :image_url
+      attributes :email, :name, :kind, :bio, :image_url, :password
 
       def self.updatable_fields(context)
         if context[:user].normal?
           super - [:kind]
+        else
+          super
+        end
+      end
+
+      def fetchable_fields
+        if context[:user].present?
+          super - [:password]
         else
           super
         end
