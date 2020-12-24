cbdb8c0cb7e517ff8a18e1e80d60180f64d9ac8f
Udit Agarwal <youditagarwal@gmail.com>
2020-12-24 16:52:11 +0530

    Update Password Added

--- devsnest/app/models/user.rb
+++ devsnest/app/models/user.rb
@@ -6,11 +6,6 @@ class User < ApplicationRecord
          :registerable,
          jwt_revocation_strategy: JwtBlacklist
 
-<<<<<<< HEAD
   enum kind: [:normal, :admin]
   has_many :gbu
 end
-=======
-  enum kind: { normal: 0, admin: 1 }
-end
->>>>>>> cb32c5a9e034ec8e8f09d06a053a406130c513ad
--- devsnest/app/resources/api/v1/user_resource.rb
+++ devsnest/app/resources/api/v1/user_resource.rb
@@ -3,12 +3,8 @@
 module Api
   module V1
     class UserResource < JSONAPI::Resource
-<<<<<<< HEAD
-      attributes :email, :name, :kind, :bio, :image_url
-=======
       attributes :email, :name, :kind, :bio, :image_url, :password
 
->>>>>>> cb32c5a9e034ec8e8f09d06a053a406130c513ad
       def self.updatable_fields(context)
         if context[:user].normal?
           super - [:kind]
