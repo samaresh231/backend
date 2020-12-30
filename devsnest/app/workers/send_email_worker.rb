21bbd5e0cc153ae6cee39bb212eb12f95ecceb29
Vedansh Garg <vedanshkt.sfc@gmail.com>
2020-12-30 19:41:08 +0530

    Adding sidekiq

---
+++ devsnest/app/workers/send_email_worker.rb
@@ -0,0 +1,9 @@
+class SendEmailWorker
+  include Sidekiq::Worker
+
+  def perform(user_id)
+    user = User.find_by(id: user_id)
+    puts "#{user.email} created a new user"
+    puts "Inside send email worker"
+  end
+end
