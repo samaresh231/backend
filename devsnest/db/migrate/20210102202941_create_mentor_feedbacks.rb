fe5319edfa1667d7a7fa984cdbfe28ab9e32b859
Vedansh Garg <vedanshkt.sfc@gmail.com>
2021-01-08 19:27:34 +0530

    rubocop chnages

--- devsnest/db/migrate/20210102202941_create_mentor_feedbacks.rb
+++ devsnest/db/migrate/20210102202941_create_mentor_feedbacks.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateMentorFeedbacks < ActiveRecord::Migration[6.0]
   def change
     create_table :mentor_feedbacks do |t|
--- devsnest/db/migrate/20210105105631_add_index_to_mmt.rb
+++ devsnest/db/migrate/20210105105631_add_index_to_mmt.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class AddIndexToMmt < ActiveRecord::Migration[6.0]
   def change
-  	add_index :mmts, :user_id, :unique => true
-  	add_index :mmts, :mentor_id, :unique => true
+    add_index :mmts, :user_id, unique: true
+    add_index :mmts, :mentor_id, unique: true
   end
 end
