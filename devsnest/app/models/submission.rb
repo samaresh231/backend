37a0c79f2f6d4ef35c5f836490b4962409b442ef
priyamishra2903 <priya.1721ec1106@kiet.edu>
2021-01-22 23:43:26 +0530

    user report

--- devsnest/app/models/submission.rb
+++ devsnest/app/models/submission.rb
@@ -2,4 +2,20 @@
 
 class Submission < ApplicationRecord
   enum status: %i[done notdone doubt]
+  has_one :content
+
+  def self.user_report(days, user_id)
+    total_ques = Content.where(data_type: 0).count
+
+    res = Content.joins(:submission).where(submissions: { status: 0, user_id: user_id }, contents: { data_type: 0 })
+    total_solved_ques = res.count
+
+    res = res.where({ updated_at: (Date.today - days)..(Date.today) }) if days.to_i.positive?
+    res = res.select(:parent_id).group(:parent_id).count
+
+    res['total_ques'] = total_ques
+    res['total_solved_ques'] = total_solved_ques
+
+    res
+  end
 end
--- devsnest/config/routes.rb
+++ devsnest/config/routes.rb
@@ -15,7 +15,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      jsonapi_resources :users
+      jsonapi_resources :users do
+        collection do
+          get :report
+        end
+      end
       jsonapi_resources :mmts
       jsonapi_resources :mentor_feedbacks
       jsonapi_resources :mentee_feedbacks
