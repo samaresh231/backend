faf5c5590585ea274abec50adf2835e629ebf659
ayush893 <mauryaayush893@gmail.com>
2021-01-22 12:19:36 +0530

    bug fix

--- devsnest/app/models/user.rb
+++ devsnest/app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
     id = self.id
     index = id % mentor_list.length
     mmt = Mmt.new(user_id: id, mentor_id: mentor_list[index])
+
     mmt.save
     @user = User.find_by(id: mentor_list[index])
     mentor_name = @user.name
@@ -28,31 +29,6 @@ class User < ApplicationRecord
       mmt = Mmt.find_by(user_id: user_id)
       if mmt
         mmt.mentor_id = mentor_id
-      else
-        mmt = Mmt.new(user_id: user_id, mentor_id: mentor_id)
-      end
-      mmt.save
-    end
-  end
-
-    index = id%mentorList.length()
-    mmt = Mmt.new(user_id: id, mentor_id: mentorList[index])
-    mmt.save
-    @user = User.find_by(id:mentorList[index])
-    mentor_name = @user.name
-
-  end
-
-  def self.assign_mentor_all
-    scorecard = User.get_scorecard
-    count = scorecard.count
-    # add logic for first 2 people
-    2.upto(count-1).each do |i|
-      user_id = scorecard[i]
-      mentor_id = scorecard[i-2]
-      mmt = Mmt.find_by(user_id: user_id)
-      if mmt
-         mmt.mentor_id = mentor_id
       else
         mmt = Mmt.new(user_id: user_id, mentor_id: mentor_id)
       end
