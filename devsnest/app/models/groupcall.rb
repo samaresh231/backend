d338871a728086c8f458eb6a4ca4f2fb992c0495
Vedansh Garg <vedanshkt.sfc@gmail.com>
2021-01-25 01:03:23 +0530

    solving group call

--- devsnest/app/models/groupcall.rb
+++ devsnest/app/models/groupcall.rb
@@ -15,7 +15,7 @@ class Groupcall < ApplicationRecord
     week = Date.current.cweek
     year = Date.current.year
 
-    count = 0
+    count = -1
     groups = []
     cur_group = []
 
@@ -24,7 +24,7 @@ class Groupcall < ApplicationRecord
       count += 1
       count = count%4
       if count == 0
-        groups.append(cur_group)
+        groups.append(cur_group) if cur_group != []
         cur_group = []
       end
       discord_id = User.find_by(id:gc.user_id).discord_id
