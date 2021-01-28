dac50db1c2a3fc1d4b61d3dfe79d550d2022d8f8
Vedansh Garg <vedanshkt.sfc@gmail.com>
2021-01-28 21:57:24 +0530

    changing docker compose and bugfix

--- devsnest/app/resources/api/v1/content_resource.rb
+++ devsnest/app/resources/api/v1/content_resource.rb
@@ -5,7 +5,7 @@ module Api
     class ContentResource < JSONAPI::Resource
       attributes :unique_id, :parent_id, :name, :data_type, :link, :priority, :score
 
-      filter :parent_id, default: 'algo'
+      filter :parent_id
       filter :unique_id
       def self.default_sort
         [{ field: 'priority', direction: :asc }]
