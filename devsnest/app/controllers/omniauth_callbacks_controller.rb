1abaf388823c60c7867dd32eb4c55ff153c8ead1
ayush893 <mauryaayush893@gmail.com>
2021-02-02 21:51:52 +0530

    bug fixed

--- devsnest/app/controllers/omniauth_callbacks_controller.rb
+++ devsnest/app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	def google_oauth2
-	user = User.from_omniauth(request.env['omniauth.auth'],current_user)
+	user = User.find_for_google_oauth2(request.env['omniauth.auth'],current_user)
 	if user.persisted?
 		render json: user
 	else
