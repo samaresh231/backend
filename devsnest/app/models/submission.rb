f91d682aa1bfc87d5c82c00a81ba8cd3cface6f7
Aditi <aditig006.com>
2021-02-02 23:46:14 +0530

    Report issue resolved

--- devsnest/app/models/submission.rb
+++ devsnest/app/models/submission.rb
@@ -8,13 +8,15 @@ class Submission < ApplicationRecord
     total_ques = Content.where(data_type: 0).count
 
     res = Content.joins(:submission).where(submissions: { status: 0, user_id: user_id }, contents: { data_type: 0 })
-    total_solved_ques = res.count
 
     res = res.where({ updated_at: (Date.today - days)..(Date.today) }) if days.to_i.positive?
     res = res.select(:parent_id).group(:parent_id).count
 
+    total_solved_ques = res.count
+    
     res['total_ques'] = total_ques
     res['total_solved_ques'] = total_solved_ques
+
     res
   end
 
