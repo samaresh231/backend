8b2337d90b416a8ec0d0f5789314982a3688ad59
ayush893 <mauryaayush893@gmail.com>
2021-02-04 13:57:54 +0530

    update user

--- devsnest/app/models/user.rb
+++ devsnest/app/models/user.rb
@@ -45,12 +45,14 @@ class User < ApplicationRecord
 
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
-    user = User.where(:email => data["email"]).first
+    user = User.where(:email => data["email"],:uid => access_token.uid).first
 
     # Uncomment the section below if you want users to be created if they don't exist
     unless user
         user = User.create(name: data["name"],
            email: data["email"],
+           uid: access_token.uid,
+           provider: access_token.provider,
            password: Devise.friendly_token[0,20]
         )
     end
